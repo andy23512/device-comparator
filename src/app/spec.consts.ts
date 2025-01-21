a2da1e6475344941445676da05937f570e5958ff
Tangent Chang <andy23512@gmail.com>
2025-01-22 01:03:12 +0800

    feat: add spec data

---
+++ src/app/spec.consts.ts
@@ -0,0 +1,298 @@
+import { Spec } from './spec.model';
+
+const M4G_SPEC: Spec = {
+  name: {
+    full: 'Master Forge',
+    short: 'M4G',
+  },
+  computational: {
+    modalities: 'Programmable Keys & Key Combinations',
+    possibleInputs: { symbol: '>', value: 13000000000 },
+    memory: 1000000,
+    memoryType: 'Embedded',
+    driver: 'Driverless OS',
+    configuration: 'Web Based FOSS/Libre offline',
+    microprocessor: '240 MHz Dual Core',
+    inputStyle: 'Fluid Chorded / Character Entry',
+    api: 'Open Serial API Specification',
+  },
+  electrical: {
+    routingStyle: 'AntiMatrix with Dedicated Channels',
+    antiGhosting: 'NKRO+',
+    hidKeyboardSchema: 'Extended 12 Key Schema',
+    usbHubPorts: 3,
+  },
+  mechanical: {
+    size: [228, 115, 38],
+    weight: { min: 346, max: 500 },
+    switchStyle: '3-D Snap Action',
+    actuationForce: { min: 55, max: 57 },
+    hardwareInterface: 'Picatinny Railing',
+    switchDurability: 20000000,
+    numberOf3DSwitches: 16,
+    switch: {
+      name: 'Omron Electronics D2LS-21(20M)',
+      url: 'https://eu.mouser.com/ProductDetail/Omron-Electronics/D2LS-2120M?qs=OcgtsXO%2B3gskSBgTf6V7tw%3D%3D',
+    },
+  },
+  material: {
+    shell: '5052 Aluminium Alloy',
+    baseplate: '3D-printed Plastic',
+  },
+};
+
+const CC2_SPEC: Spec = {
+  name: {
+    full: 'CharaChorder Two',
+    short: 'CC2',
+  },
+  computational: {
+    modalities: 'Programmable Keys & Key Combinations',
+    possibleInputs: { symbol: '>', value: 13000000000 },
+    memory: 60000,
+    memoryType: 'Embedded',
+    driver: 'Driverless OS',
+    configuration: 'Web Based FOSS/Libre offline',
+    microprocessor: '240 MHz Dual Core',
+    inputStyle: 'Fluid Chorded / Character Entry',
+    api: 'Open Serial API Specification',
+  },
+  electrical: {
+    routingStyle: 'AntiMatrix with Dedicated Channels',
+    antiGhosting: 'NKRO+',
+    hidKeyboardSchema: 'Extended 12 Key Schema',
+    usbHubPorts: 'X',
+  },
+  mechanical: {
+    size: [295, 111, 29],
+    weight: 303,
+    switchStyle: '3-D Tactile',
+    actuationForce: 42,
+    hardwareInterface: 'Female ¼"-20 Thread',
+    switchDurability: 1000000,
+    numberOf3DSwitches: 18,
+    switch: {
+      name: 'Alps Alpine SKRHADE010',
+      url: 'https://tech.alpsalpine.com/e/products/detail/SKRHADE010/',
+    },
+  },
+  material: {
+    shell: 'Injection Molded ABS Plastic',
+    baseplate: '3D-printed Plastic',
+  },
+};
+
+const CC1_SPEC: Spec = {
+  name: {
+    full: 'CharaChorder One',
+    short: 'CC1',
+  },
+  computational: {
+    modalities: 'Programmable Keys & Key Combinations',
+    possibleInputs: { symbol: '>', value: 13000000000 },
+    memory: 16000,
+    memoryType: 'Embedded',
+    driver: 'Driverless OS',
+    configuration: 'Web Based FOSS/Libre offline',
+    microprocessor: '48 MHz',
+    inputStyle: 'Fluid Chorded / Character Entry',
+    api: 'Open Serial API Specification',
+  },
+  electrical: {
+    routingStyle: 'AntiMatrix with Dedicated Channels',
+    antiGhosting: 'NKRO+',
+    hidKeyboardSchema: 'Extended 12 Key Schema',
+    usbHubPorts: 'X',
+  },
+  mechanical: {
+    size: [296, 110, 38],
+    weight: 308,
+    switchStyle: '3-D Tactile',
+    actuationForce: 70,
+    hardwareInterface: 'N/A',
+    switchDurability: 200000,
+    numberOf3DSwitches: 18,
+    switch: {
+      name: '???',
+      url: null,
+    },
+  },
+  material: {
+    shell: 'Injection Molded ABS Plastic',
+    baseplate: 'Injection Molded ABS Plastic',
+  },
+};
+
+const CCL_SPEC: Spec = {
+  name: {
+    full: 'CharaChorder Lite',
+    short: 'CCL',
+  },
+  computational: {
+    modalities: 'Programmable Keys & Key Combinations',
+    possibleInputs: { symbol: '>', value: 17000000000 },
+    memory: 65000,
+    memoryType: 'Embedded',
+    driver: 'Driverless OS',
+    configuration: 'Web Based FOSS/Libre offline',
+    microprocessor: '???',
+    inputStyle: 'Fluid Chorded / Character Entry',
+    api: 'Open Serial API Specification',
+  },
+  electrical: {
+    routingStyle: 'AntiMatrix with Dedicated Channels',
+    antiGhosting: 'NKRO+',
+    hidKeyboardSchema: 'Extended 12 Key Schema',
+    usbHubPorts: 'X',
+  },
+  mechanical: {
+    size: [295, 115, 33],
+    weight: 468,
+    switchStyle: '1-D Mechanical',
+    actuationForce: 35,
+    hardwareInterface: 'N/A',
+    switchDurability: '???',
+    numberOf3DSwitches: 'N/A',
+    switch: {
+      name: 'Gateron Clear/White',
+      url: null,
+    },
+  },
+  material: {
+    shell: 'ABS Plastic',
+    baseplate: 'Plastic',
+  },
+};
+
+const CCX_SPEC: Spec = {
+  name: {
+    full: 'CharaChorder X',
+    short: 'CCX',
+  },
+  computational: {
+    modalities: 'Programmable Keys & Key Combinations',
+    possibleInputs: { symbol: '>', value: 17000000000 },
+    memory: 65000,
+    memoryType: 'Embedded',
+    driver: 'Driverless OS',
+    configuration: 'Web Based FOSS/Libre offline',
+    microprocessor: '???',
+    inputStyle: 'Fluid Chorded / Character Entry',
+    api: 'Open Serial API Specification',
+  },
+  electrical: {
+    routingStyle: 'AntiMatrix with Dedicated Channels',
+    antiGhosting: 'N/A',
+    hidKeyboardSchema: 'Extended 12 Key Schema',
+    usbHubPorts: 'X',
+  },
+  mechanical: {
+    size: [59, 23.25, 16.15],
+    weight: '???',
+    switchStyle: 'N/A',
+    actuationForce: 'N/A',
+    hardwareInterface: 'N/A',
+    switchDurability: 'N/A',
+    numberOf3DSwitches: 'N/A',
+    switch: {
+      name: 'N/A',
+      url: null,
+    },
+  },
+  material: {
+    shell: 'Injection Molded Plastic',
+    baseplate: 'N/A',
+  },
+};
+
+const SVAL_SPEC: Spec = {
+  name: {
+    full: 'Svalboard',
+    short: 'Sval',
+  },
+  computational: {
+    modalities: 'Programmable Keys & Key Combinations',
+    possibleInputs: '???',
+    memory: '???',
+    memoryType: 'Embedded',
+    driver: '???',
+    configuration: 'Web Based FOSS/Libre offline',
+    microprocessor: '133 MHz Dual Core',
+    inputStyle: '???',
+    api: '???',
+  },
+  electrical: {
+    routingStyle: '???',
+    antiGhosting: 'NKRO',
+    hidKeyboardSchema: '???',
+    usbHubPorts: '???',
+  },
+  mechanical: {
+    size: [190, 130, 90],
+    weight: 550,
+    switchStyle: '3-D Magneto-optical',
+    actuationForce: 20,
+    hardwareInterface: 'Female M5 and ¼"-20 Thread',
+    switchDurability: '???',
+    numberOf3DSwitches: 10,
+    switch: {
+      name: '???',
+      url: null,
+    },
+  },
+  material: {
+    shell: 'PLA or ABS Plastic',
+    baseplate: 'N/A',
+  },
+};
+
+const MARKET_LEADING_KEYBOARD_SPEC: Spec = {
+  name: {
+    full: 'Market Leading Keyboard',
+    short: null,
+  },
+  computational: {
+    modalities: 'Individually Programmable Keys Only',
+    possibleInputs: { symbol: '<', value: 200 },
+    memory: 'Uses Host CPU Resources',
+    memoryType: 'Uses Host CPU Resources',
+    driver: 'Requires Custom Driver from Host CPU',
+    configuration: 'Requires Proprietary Software',
+    microprocessor: '150 MHz Single Core',
+    inputStyle: 'Character Entry Only',
+    api: 'None',
+  },
+  electrical: {
+    routingStyle: 'Traditional Key Matrix',
+    antiGhosting: 'NKRO',
+    hidKeyboardSchema: '6 Key Schema',
+    usbHubPorts: { min: 0, max: 1 },
+  },
+  mechanical: {
+    size: [450, 158, 38],
+    weight: 989,
+    switchStyle: '1-D Mechanical / Optical / Halls',
+    actuationForce: { min: 40, max: 80 },
+    hardwareInterface: 'N/A',
+    switchDurability: '???',
+    numberOf3DSwitches: 'N/A',
+    switch: {
+      name: '???',
+      url: null,
+    },
+  },
+  material: {
+    shell: '???',
+    baseplate: '???',
+  },
+};
+
+export const SPECS: Spec[] = [
+  M4G_SPEC,
+  CC2_SPEC,
+  CC1_SPEC,
+  CCL_SPEC,
+  CCX_SPEC,
+  SVAL_SPEC,
+  MARKET_LEADING_KEYBOARD_SPEC,
+];
---
+++ src/app/spec.model.ts
@@ -0,0 +1,47 @@
+export interface Spec {
+  name: {
+    full: string;
+    short: string | null;
+  };
+  computational: {
+    modalities:
+      | 'Programmable Keys & Key Combinations'
+      | 'Individually Programmable Keys Only';
+    possibleInputs: { symbol: '<' | '>'; value: number } | '???';
+    memory: number | 'Uses Host CPU Resources' | '???';
+    memoryType: 'Embedded' | 'Uses Host CPU Resources';
+    driver: 'Driverless OS' | 'Requires Custom Driver from Host CPU' | '???';
+    configuration:
+      | 'Web Based FOSS/Libre offline'
+      | 'Requires Proprietary Software';
+    microprocessor: string;
+    inputStyle:
+      | 'Fluid Chorded / Character Entry'
+      | 'Character Entry Only'
+      | '???';
+    api: 'Open Serial API Specification' | 'None' | '???';
+  };
+  electrical: {
+    routingStyle:
+      | 'AntiMatrix with Dedicated Channels'
+      | 'Traditional Key Matrix'
+      | '???';
+    antiGhosting: 'NKRO+' | 'NKRO' | 'N/A';
+    hidKeyboardSchema: 'Extended 12 Key Schema' | '6 Key Schema' | '???';
+    usbHubPorts: { min: number; max: number } | number | 'X' | '???';
+  };
+  mechanical: {
+    size: [number, number, number];
+    weight: { min: number; max: number } | number | '???';
+    switchStyle: string;
+    actuationForce: { min: number; max: number } | number | 'N/A';
+    hardwareInterface: string;
+    switchDurability: number | '???' | 'N/A';
+    numberOf3DSwitches: number | 'N/A';
+    switch: { name: string; url: string | null };
+  };
+  material: {
+    shell: string;
+    baseplate: string;
+  };
+}
